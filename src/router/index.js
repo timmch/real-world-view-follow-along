97f75162060777fdda56682375ec5ac7bd906fe2
Tim McHugh <tim@startwithwhy.com>
2020-04-15 10:27:48 -0400

    Initial commit after lesson 4

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,42 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
+    path: '/',
+    name: 'event-list',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ '../views/EventList.vue')
+  },
+  {
+    path: '/event',
+    name: 'event-show',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/EventShow.vue')
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/EventCreate.vue')
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
---
+++ src/views/EventList.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Events Listing</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
